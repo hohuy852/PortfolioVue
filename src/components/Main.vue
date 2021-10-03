d753c549e7a408d115c7159f7b6dc254d222bdf9
Your Name <syhuy851@gmail.com>
2021-10-03 17:44:58 +0700

    v 1.2.1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -34,7 +34,7 @@
               </v-list-item>
             </v-list>
             <!-- Social -->
-            <div class="title mb-1">Socials</div>
+            <div class="title mb-1">SOCIALS</div>
             <v-divider></v-divider>
             <v-list>
               <v-list-item v-for="item in socials" :key="item.title">
@@ -54,7 +54,7 @@
               </v-list-item>
             </v-list>
             <!-- Hobbies -->
-            <div class="title mb-1">Hobbies</div>
+            <div class="title mb-1">HOBBIES</div>
             <v-divider></v-divider>
             <div class="text-center mt-3">
               <v-chip
@@ -114,12 +114,24 @@
             <v-card-title> Who am i ? </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              I'm a developer born in Nghe An, Vietnam
+              I'm a developer born in Nghe An, Vietnam. I'm a pretty introverted
+              person
+            </v-card-text>
+            <v-card-text>
+              Why did I choose IT? A question that is quite difficult for me to
+              answer, well, I was born in a poor country, to be exposed to
+              technology for me is quite a luxury. But everything changed when I
+              moved south, and then I got to see for the first time this thing
+              called a computer, which got me hooked, excited with all the
+              wonderful things it had to offer. And from there my passion for
+              computers grew. The older I get, the more I want to learn more
+              about it, how it is made, how it works... .And now the passion for
+              it is still burning inside of me.
             </v-card-text>
             <v-card-text>
               I'm interested and skilled in different topics of Information
               Technology including programming, web development, and computer
-              and network security
+              and network security and I've always wanted to find a job related to something I'm passionate about
             </v-card-text>
             <!-- What am i proud of ? -->
             <v-card-title> What am i proud of ? </v-card-title>
@@ -313,52 +325,52 @@ export default {
         {
           icon: "mdi-bullseye",
           content: "Getting out of Safe Zone",
-          color: '#E57373'
+          color: "#E57373",
         },
         {
           icon: "mdi-biohazard",
           content: "Challenges",
-          color: '#FF80AB'
+          color: "#FF80AB",
         },
         {
           icon: "mdi-bike",
           content: "Cycling",
-          color: '#CE93D8'
+          color: "#CE93D8",
         },
         {
           icon: "mdi-image-filter-hdr",
           content: "Nature",
-          color: '#2E7D32'
+          color: "#2E7D32",
         },
         {
           icon: "mdi-music",
           content: "Music",
-          color: '#B39DDB'
+          color: "#B39DDB",
         },
         {
           icon: "mdi-gamepad-variant",
           content: "Games",
-          color: '#80DEEA'
+          color: "#80DEEA",
         },
         {
           icon: "mdi-airplane-takeoff",
           content: "Travel",
-          color: '#26A69A'
+          color: "#26A69A",
         },
         {
           icon: "mdi-book",
           content: "Book",
-          color: '#40C4FF'
+          color: "#40C4FF",
         },
         {
           icon: "mdi-account-group",
           content: "Leadership",
-          color: '#DCE775'
+          color: "#DCE775",
         },
         {
           icon: "mdi-bug",
           content: "Making bugs!",
-          color: '#D7CCC8'
+          color: "#D7CCC8",
         },
       ],
       info: [
